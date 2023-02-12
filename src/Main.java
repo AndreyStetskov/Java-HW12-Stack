804bfffb3d5ff3ad9b74fdfc1864a9fb73948042
Andrey <stsiatskou@gmail.com>
2023-02-12 17:22:32 +0100

    Initial homework12

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a message: ");
+        String textForDecryption = scan.nextLine();
+
+        System.out.println("Enter a key: ");
+        int key = scan.nextInt();
+
+        Decoder.decoder(textForDecryption, key);
+    }
+}
